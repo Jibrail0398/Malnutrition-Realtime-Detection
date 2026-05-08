51fb460c978a245b7d024bc68560d60e99f0f2a8
Jibrail0398 <izamikatsuka@gmail.com>
2026-05-08 15:59:54 +0700

    load model

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import Camera from './Camera'
+import { useState, useEffect } from 'react';
+import Camera from './Camera';
+import { useModel } from './hooks/useModel';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  
+  const modelURL = `${import.meta.env.BASE_URL}best.onnx`;
+  const { session, loading } = useModel(modelURL);
+  
 
   return (
     <>
     
-
+    {loading?(
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h2>Sedang memuat model YOLOv10...</h2>
+      </div>
+    ):(
       <Camera/>
+    )}
 
     </>
   )
---
+++ src/hooks/useModel.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import * as ort from "onnxruntime-web";
+
+ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
+
+
+export function useModel(modelURL){
+    const [session,setSession]= useState(null);
+    const [loading,setLoading]= useState(true);
+
+    useEffect(()=>{
+
+        async function loadModel(){
+            setLoading(true);
+            try{
+              
+                
+                const mySession = await ort.InferenceSession.create(modelURL, {
+                executionProviders: ['wasm'],
+                graphOptimizationLevel: 'all'
+                });
+
+                setSession(mySession);
+                
+    
+            }catch(e){
+                console.log(`Terjadi error saat load model: ${e}`);
+            }finally{
+                setLoading(false)
+            }
+        }
+        loadModel();
+    },[modelURL]);
+
+    return {session,loading};
+}
